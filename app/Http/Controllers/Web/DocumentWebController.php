319290dfd37dfb4b0cba1747bd32c7789313d412
Bronyrus <bronyruschannel@gmail.com>
2019-12-17 16:47:53 +0300

    Update DocumentWebController.php

--- app/Http/Controllers/Web/DocumentWebController.php
+++ app/Http/Controllers/Web/DocumentWebController.php
@@ -33,43 +33,18 @@ class DocumentWebController extends Controller
      */
     public function store(Request $request)
     {
+        foreach($request->file('docs') as $file)
+        {
+            $path = $file->store('public/newsPictures');
+            $url = Storage::url($path);
 
-        dd($request->file('docs'));
-
-        // $path = $request->file('news_picture')->store('public/newsPictures');
-        // $url = Storage::url($path);
-
-        return 'a';
-
-        $request->news_head = trim($request->news_head);
-        $request->news_body = trim($request->news_body);
-
-        $validator = Validator::make($request->all(), [
-            'news_head' => 'required|min:3|max:100|string',
-            'news_body' => 'required|min:3|max:20000',
-            'news_picture' => 'required|image|mimes:jpeg,png,jpg,gif,svg'
-        ]);
-
-        if ($validator->fails()) {
-            return redirect()
-                ->route('auth.news.create')
-                ->withErrors($validator)
-                ->withInput();
+            Document::create([
+                'section' => Str::uuid(),
+                'doc' => $url,
+            ]);
         }
 
-        // return $request->news_body . ' ' . $request->news_head;
-
-        $path = $request->file('news_picture')->store('public/newsPictures');
-        $url = Storage::url($path);
-
-        News::create([
-            'uuid' => Str::uuid(),
-            'head' => $request->input('news_head'),
-            'body' => $request->input('news_body'),
-            'picture' => $url,
-        ]);
-
-        return redirect()->route('auth.news.index');
+        return redirect()->route('auth.documents.index');
     }
 
     /**
