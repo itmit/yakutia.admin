33a5d580c514f5e21e15158ad9ca8b3cf33f33be
Bronyrus <bronyruschannel@gmail.com>
2019-12-18 11:16:58 +0300

    cases delete

--- app/Http/Controllers/Web/CasesWebController.php
+++ app/Http/Controllers/Web/CasesWebController.php
@@ -77,47 +77,14 @@ class CasesWebController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request)
     {
-        //
+        Cases::where('id', '=', $request->id)->delete();
+        return response()->json(['succses'=>'Удалено'], 200); 
     }
 }
--- resources/views/cases/caseList.blade.php
+++ resources/views/cases/caseList.blade.php
@@ -10,7 +10,7 @@
 
     <div class="row">
         <div class="col-sm-12">
-                <h1>{{ $item->head }}</h1>
+                <h1>{{ $item->head }}<i class="material-icons delete-case" style="cursor: pointer" data-id="{{ $item->id }}">delete</i></h1>
             <div class="row">
             <div class="col-4 col-sm-12">
                 <img src="{{ $item->picture }}" alt="{{ $item->head }}" width="35%" style="float:left; margin: 7px 7px 7px 0;">
@@ -21,5 +21,32 @@
     </div>
     
     @endforeach
+
+<script>
+
+$(document).on('click', '.delete-news', function() {
+    let isDelete = confirm("Удалить новость? Данное действие невозможно отменить!");
+
+    if(isDelete)
+    {
+        let id = $(this).data('id');
+        $.ajax({
+            headers : {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+            dataType: "json",
+            data    : { id: id },
+            url     : 'cases/delete',
+            method    : 'delete',
+            success: function (response) {
+                $(this).closest('.row').remove();
+                console.log('Удалено!');
+            },
+            error: function (xhr, err) { 
+                console.log("Error: " + xhr + " " + err);
+            }
+        });
+    }
+});
+
+</script>
 
 @endsection
