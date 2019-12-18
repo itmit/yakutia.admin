ff06fc068020ec7abcced826b06bc2e82270a86d
Bronyrus <bronyruschannel@gmail.com>
2019-12-18 12:30:52 +0300

    Update Messenger.php

--- app/Models/Messenger.php
+++ app/Models/Messenger.php
@@ -28,6 +28,6 @@ class Messenger extends Model
 
     public function lastMessageDirection()
     {
-        return $this->hasOne(UserToMessage::class, 'id')->latest()->first('direction');
+        return $this->hasOne(UserToMessage::class, 'id')->first('direction');
     }
 }
