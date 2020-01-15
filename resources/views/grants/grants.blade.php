f0dcdad5d45794ec5d9df8191583945f7b456e50
Bronyrus <bronyruschannel@gmail.com>
2020-01-15 12:00:58 +0300

    Update grants.blade.php

--- resources/views/grants/grants.blade.php
+++ resources/views/grants/grants.blade.php
@@ -6,7 +6,7 @@
     <div class="row justify-content-center cont-m">
         <div class="col-md-12">
             <form class="form-horizontal" method="POST" action="{{ route('auth.grants.store') }}" enctype="multipart/form-data">
-                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <textarea name="" id="" cols="30" rows="10" style="width: 100%"></textarea>
             </form>
         </div>
     </div>
