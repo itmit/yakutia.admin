c297795d5f6b1be4be0f79d0d34f5bda8037c232
Bronyrus <bronyruschannel@gmail.com>
2020-01-15 12:18:21 +0300

    Update grants.blade.php

--- resources/views/grants/grants.blade.php
+++ resources/views/grants/grants.blade.php
@@ -6,6 +6,7 @@
     <div class="row justify-content-center cont-m">
         <div class="col-md-12">
             <form class="form-horizontal" method="POST" action="{{ route('auth.grants.store') }}" enctype="multipart/form-data">
+                {{ csrf_field() }}
                 <textarea name="grant" style="width: 100%">{{ $grant }}</textarea>
                 <input type="submit" value="Сохранить">
             </form>
