b26645dcc36abefe4eb05a842e01344aebe2fad0
Bronyrus <bronyruschannel@gmail.com>
2020-01-15 09:47:58 +0300

    Update ContactWebController.php

--- app/Http/Controllers/Web/ContactWebController.php
+++ app/Http/Controllers/Web/ContactWebController.php
@@ -20,7 +20,7 @@ class ContactWebController extends Controller
     {
         return view('contacts.contactsList', [
             'title' => 'Конкурсы',
-            'contacts' => Contact::all()->sortBy('created_at', 'desc')
+            'contacts' => Contact::all()->sortBy('created_at')
         ]);
     }
 
