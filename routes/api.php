8c1ea67d90a2dabe30798c705e97d71d9d2267dd
Bronyrus <bronyruschannel@gmail.com>
2020-01-15 10:07:11 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -37,5 +37,7 @@ Route::group(['middleware' => 'auth:api'], function(){
     Route::post('poll/getPollQuestionList', 'Api\PollApiController@getPollQuestionList');
     Route::post('poll/passPoll', 'Api\PollApiController@passPoll');
     
-    Route::get('contacts/index', 'Api\ContactWebController@index');
+    // Route::get('contacts/index', 'Api\ContactWebController@index');
 });
+
+Route::get('contacts/index', 'Api\ContactWebController@index');
