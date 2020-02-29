b6fd37a2d32046e8149bf1673829cb2625f431d0
Bronyrus <bronyruschannel@gmail.com>
2020-02-29 14:55:34 +0300

    Update newsEdit.blade.php

--- resources/views/news/newsEdit.blade.php
+++ resources/views/news/newsEdit.blade.php
@@ -11,7 +11,7 @@
         </div>
 
         <div class="col-xs-12 col-sm-10">
-            <input id="news_head" type="text" class="form-control" name="news_head" value="{{ old('news_head') }} {{ $news->news_head }}" required maxlength="191">
+            <input id="news_head" type="text" class="form-control" name="news_head" value="{{ old('news_head') }} {{ $news->head }}" required maxlength="191">
 
             @if ($errors->has('news_head'))
                 <span class="help-block">
@@ -27,7 +27,7 @@
         </div>
 
         <div class="col-xs-12 col-sm-10">
-            <textarea id="news_body" type="text" class="md-textarea form-control" name="news_body" cols="30" rows="10" maxlength="2000">{{ old('news_body') }} {{ $news->news_body }}</textarea>
+            <textarea id="news_body" type="text" class="md-textarea form-control" name="news_body" cols="30" rows="10" maxlength="2000">{{ old('news_body') }} {{ $news->body }}</textarea>
 
             @if ($errors->has('news_body'))
                 <span class="help-block">
@@ -43,7 +43,7 @@
         </div>
     
         <div class="col-xs-12 col-sm-10">
-            <input type="file" name="news_picture" id="news_picture" class="form-control-file" accept="image/*" required value="{{ $news->news_picture }}">
+            <input type="file" name="news_picture" id="news_picture" class="form-control-file" accept="image/*" required value="{{ $news->icture }}">
 
             @if ($errors->has('news_picture'))
                 <span class="help-block">
