b6ac736a5d1efded5151deed2a26120b93715cea
Bronyrus <bronyruschannel@gmail.com>
2020-03-02 16:09:56 +0300

    Update sendEvent.php

--- app/Console/Commands/sendEvent.php
+++ app/Console/Commands/sendEvent.php
@@ -73,5 +73,38 @@ class sendEvent extends Command
                 curl_close ( $ch );
             };
         };
+
+        $tomorrow = date("Y-m-d", strtotime("+1 day"));  
+        $events = Event::where('date_start', $tomorrow)->get();
+
+        foreach ($events as $event) {
+            $users = UserToEvent::where('event_id', $event->id)->get();
+            foreach ($users as $user) {
+                $fields = array (
+                    'to' => $user->user()->device_token,
+                    "notification" => [
+                        "body" => "Завтра событие " . $event->head,
+                        "title" => "Внимание"
+                    ]
+                );
+                $fields = json_encode ( $fields );
+        
+                $headers = array (
+                        'Authorization: key=' . "AAAA6ySBDpw:APA91bH_y7bFtB0fHFyLiSiDjvy4BvqzkiOzsU_QbJyWFHAH0n1EdGqsllWm_r_wOxDGxiThbHtLRVF7WzaG3pZFTp_Skxk9bb-VeZdA8HOwIQG7hOvZb4LhOWqjX6sV9nkaHhbzpgzp",
+                        'Content-Type: application/json'
+                );
+        
+                $ch = curl_init ();
+                curl_setopt ( $ch, CURLOPT_URL, $url );
+                curl_setopt ( $ch, CURLOPT_POST, true );
+                curl_setopt ( $ch, CURLOPT_HTTPHEADER, $headers );
+                curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
+                curl_setopt ( $ch, CURLOPT_POSTFIELDS, $fields );
+        
+                curl_exec ( $ch );
+        
+                curl_close ( $ch );
+            };
+        };
     }
 }
