647e872295f9fad3b5c047d925bd5cd6c23eb80a
Bronyrus <bronyruschannel@gmail.com>
2020-03-02 12:30:26 +0300

    Update moreGrant.blade.php

--- resources/views/moreGrant/moreGrant.blade.php
+++ resources/views/moreGrant/moreGrant.blade.php
@@ -5,7 +5,7 @@
 <div class="col-sm-12 tabs-content">
     <div class="row justify-content-center cont-m">
         <div class="col-md-12">
-            <form class="form-horizontal" method="POST" action="g{{ $t }}" enctype="multipart/form-data">
+            <form class="form-horizontal" method="POST" action="g{{ $t }}.store" enctype="multipart/form-data">
                 {{ csrf_field() }}
                 <textarea name="grant" style="width: 100%">@if($grant){{ $grant->grant }}@endif</textarea>
                 <div class="row form-group{{ $errors->has('more_grant_files') ? ' has-error' : '' }}">
