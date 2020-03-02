8e3a9c52b908e0af6eba07c1da718aa53dbab197
Bronyrus <bronyruschannel@gmail.com>
2020-03-02 11:49:59 +0300

    Update adminApp.blade.php

--- resources/views/layouts/adminApp.blade.php
+++ resources/views/layouts/adminApp.blade.php
@@ -117,9 +117,9 @@
                             <li name="grants" style="width: 100%"><a href="{{ route('auth.grants.index') }}">Президентские гранты</a></li>
 
                             <ul>
-                                <li name="g1" style="width: 100%"><a href="{{ route('auth.g1.index') }}">Для победителей</a></li>
-                                <li name="g2" style="width: 100%"><a href="{{ route('auth.g2.index') }}">Для участников</a></li>
-                                <li name="g3" style="width: 100%"><a href="{{ route('auth.g3.index') }}">Список победителей с РС (Я)</a></li>
+                                <li name="g1" style="width: 100%"><a href="{{ route('auth.g1') }}">Для победителей</a></li>
+                                <li name="g2" style="width: 100%"><a href="{{ route('auth.g2') }}">Для участников</a></li>
+                                <li name="g3" style="width: 100%"><a href="{{ route('auth.g3') }}">Список победителей с РС (Я)</a></li>
                             </ul>
 
                             <li name="contacts" style="width: 100%"><a href="{{ route('auth.contacts.index') }}">Контакты</a></li>
